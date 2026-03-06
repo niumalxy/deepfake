a1bb3a8f9378eafd2b5a5e5debddce8bccbe8dcf
Thisi <thisislxya@163.com>
2026-03-06 15:05:51 +0800

    feat: async execute dump

--- segment_agent/nodes/dump2db/save.py
+++ segment_agent/nodes/dump2db/save.py
@@ -5,8 +5,7 @@ from segment_agent.graph.state import AgentState
 from utils.img_convert import img_to_base64
 from mq.reflection_produce import reflection_produce
 from entity.dump_type import DumpType
-
-
+import asyncio
 
 # 保存分析结果到数据库
 def dump2db(state: AgentState, config: SegmentAgentConfig) -> None:
@@ -17,44 +16,44 @@ def dump2db(state: AgentState, config: SegmentAgentConfig) -> None:
         state: 工作流状态
         config: 配置参数
     """
-    analysis = {
-        "task_id": config["task_id"],
-        "origin_img": img_to_base64(state["origin_img"]),
-        "label": config["label"][0],
-        "description": config["label"][1],
-        "prediction": state["prediction"],
-        "report": state["report"]
-    }
-    # 如果没有可疑区域，直接返回
+    # 如果没有可疑区域，需要补充normal标签
     if not state["cropped_imgs"]:
-        state["prediction"] == "normal"
-    
+        state["prediction"] = "normal"
+
     # 如果结果正确，或状态为INVALID，暂时不分析
-    if ((state["prediction"] == "fake") ^ (config["label"][0] == 0
-    )) or state["status"] == AgentStatus.INVALID:
+    if ((state["prediction"] == "fake") ^ (config["label"][0] == 0)) or state["status"] == AgentStatus.INVALID:
         return
 
+    async def dump_analysis():
+        analysis = {
+            "task_id": config["task_id"],
+            "origin_img": img_to_base64(state["origin_img"]),
+            "label": config["label"][0],
+            "description": config["label"][1],
+            "prediction": state["prediction"],
+            "report": state["report"]
+        }
+        # 如果结果不对，需要分析，先存入MongoDB，再在MQ中生产该记录
+        insert_analysis(analysis)
 
+        # 先做处理
+        segment_images = []
+        for item in state["cropped_imgs"]:
+            segment_images.append({
+                "top_left": item["top_left"],
+                "bottom_right": item["bottom_right"],
+                "description": item["description"],
+            })
+        segment = {
+            "task_id": config["task_id"],
+            "origin_img": img_to_base64(state["origin_img"]),
+            "segment_images": segment_images,
+        }
+        # 保存分割结果
+        insert_segment(segment)
 
-    # 如果结果不对，需要分析，先存入MongoDB，再在MQ中生产该记录
-    insert_analysis(analysis)
-
-    # 先做处理
-    segment_images = []
-    for item in state["cropped_imgs"]:
-        segment_images.append({
-            "top_left": item["top_left"],
-            "bottom_right": item["bottom_right"],
-            "description": item["description"],
-        })
-    segment = {
-        "task_id": config["task_id"],
-        "origin_img": img_to_base64(state["origin_img"]),
-        "segment_images": segment_images,
-    }
-    # 保存分割结果
-    insert_segment(segment)
-
-    # MQ生产消息
-    reflection_produce({"task_id": config["task_id"]}, DumpType.SEGMENT)
-    reflection_produce({"task_id": config["task_id"]}, DumpType.REPORT)
+        # MQ生产消息
+        reflection_produce({"task_id": config["task_id"]}, DumpType.SEGMENT)
+        reflection_produce({"task_id": config["task_id"]}, DumpType.REPORT)
+    
+    asyncio.run(dump_analysis())
--- segment_agent/nodes/img_part_analysis/prompt.py
+++ segment_agent/nodes/img_part_analysis/prompt.py
@@ -1,5 +1,4 @@
-with open("segment_agent/docs/segment_constitution.md", "r") as f:
-    PARTIAL_IMAGE_ANALYSIS_PROMPT = f.read()
-
 def get_partial_image_analysis_prompt() -> str:
+    with open("segment_agent/docs/constitution.md", "r") as f:
+        PARTIAL_IMAGE_ANALYSIS_PROMPT = f.read()
     return PARTIAL_IMAGE_ANALYSIS_PROMPT
